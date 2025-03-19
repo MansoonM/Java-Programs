af115f80137810fbd3b83c8de14fab052d462818
Mansoon Mohanty <136214687+MansoonM@users.noreply.github.com>
2025-03-19 11:48:40 +0530

    Add files via upload

---
+++ 02_Operators_Expression/operators.java
@@ -0,0 +1,25 @@
+public class operators {
+    public static void main(String[] args) {
+        System.out.println("Operators: There are 8 Operators in java.");
+
+        // 1. Arithmetic Operators ( +, -, *, /, %)
+        int num1 = 40;
+        int num2 = 20;
+        System.out.println("Addition of num1(40) and num2(20): "+ (num1 + num2));
+        System.out.println("Substraction of num1(40) and num2(20): "+ (num1 - num2));
+        System.out.println("Multiplication of num1(40) and num2(20): "+ (num1 * num2));
+        System.out.println("Division of num1(40) and num2(20): "+ (num1 / num2));
+        System.out.println("Remainder of num1(40) and num2(20): "+ (num1 % num2));
+
+    }
+}
+// There are 8 Operators in Java
+
+//   1. Arithmetic Operators ( +, -, *, /, %)
+//   2. Relational Operators ( <, >, <=, >=, ==, !=)
+//   3. Logical Operators ( &&, ||, ! )
+//   4. Assignment Operators (=, -=, +=, *=, /=, %=)
+//   5. Increment Decrement Operators (++,--)
+//   6. Conditional Operators (exp1 ? exp2 : exp3)
+//   7. Bitwise Operators (&, !, ^, ~, >>, <<, >>>)
+//   8. Special Operators (.)
